201d64ac96c98da940daf997adbdae69d5007c2a
Alexandre-Masse <alexandremasse06@hotmail.com>
2023-11-13 12:00:00 -0500

    Programation scene

---
+++ Assets/Scripts/CommencerLeJeu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CommencerLeJeu : MonoBehaviour
+{
+    public void CommencerLePartie()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
